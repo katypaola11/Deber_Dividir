df932b7995c869c161af4586d622c48f0b6bcc96
katypaola11 <kpantoja1711@gmail.com>
2025-02-06 10:18:22 -0500

    objeto

--- src/screens/CalculoScreen.tsx
+++ src/screens/CalculoScreen.tsx
@@ -1,44 +1,54 @@
-import React, { useState } from 'react'
-import { View, Text, Button } from 'react-native'
-import { InputScreen } from '../components/InputScreen'
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { InputScreen } from '../components/InputScreen';
 import { ButtonScreen } from '../components/ButtonScreen';
 import { styles } from '../theme/appTheme';
 import { CommonActions, useNavigation } from '@react-navigation/native';
 
-
+interface Numbers {
+    num1: string; 
+    num2: string;
+}
 
 export const CalculoScreen = () => {
     
     const navigation = useNavigation();
 
-    const [num1, setNum1] = useState('');
-    const [num2, setNum2] = useState('');
-    const [resultado, setResultado] = useState('');
+    const [numbers, setNumbers] = useState<Numbers>({
+        num1: '',
+        num2: '',
+    });
+
+    const [resultado, setResultado] = useState<string>('');  
 
     const handleDivision = () => {
-        const n1 = parseFloat(num1);
-        const n2 = parseFloat(num2);
-        if (n1===0 &&n2 === 0) {
+        const n1 = parseFloat(numbers.num1);
+        const n2 = parseFloat(numbers.num2);
+        
+        if (!numbers.num1 || !numbers.num2) {
+            setResultado('Por favor ingrese ambos números');
+            return;
+        }
+
+        if (n1 === 0 && n2 === 0) {
             setResultado('Indeterminado');
-        } else if (n2 === 0) {
-            setResultado('No se puede dividir para cero');
-        } else {
+        }else if ( n2 === 0) {
+            setResultado('no se puede dividir por 0');
+        }else {
             setResultado((n1 / n2).toString());
         }
-    }
+    };
 
-  return (
-    <View style={styles.conteiner}>
-        <InputScreen  placeholder="Numero 1"  onChangeText={(text) => setNum1(text)}  />
-        <InputScreen placeholder="Numero 2" onChangeText={(text) => setNum2(text)}  />
+    return (
+        <View style={styles.conteiner}>
+            <InputScreen placeholder="campo 1"  onChangeText={(text) => setNumbers({ ...numbers, num1: text })}  />
+            <InputScreen placeholder="campo 2"  onChangeText={(text) => setNumbers({ ...numbers, num2: text })}  />
 
-        <ButtonScreen title="Dividir" onChangeText={handleDivision}   />
-        <Text style={styles.text1}>Resultado: {resultado}</Text>
-       
+            <ButtonScreen title="Dividir" onChangeText={handleDivision} /> 
+            
+            <Text style={styles.text1}>Resultado: {resultado}</Text>
 
-        <Button  title='Regresar'  onPress={() => navigation.dispatch(CommonActions.navigate({ name: 'Home' }))}/>
-    </View>
-  )
-}
-
-
+            <Button   title='Regresar'  onPress={() => navigation.dispatch(CommonActions.navigate({ name: 'Home' }))} />
+        </View>
+    );
+};
